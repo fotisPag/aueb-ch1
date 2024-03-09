84d822041b7fdd5b17b040ee664da8ab925004ef
fotisPag <fotis.pagonis01@gmail.com>
2024-03-09 20:15:22 +0200

    added variables demo

---
+++ VariablesDemo.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.ch1;
+
+public class VariablesDemo {
+
+    public static void main(String[] args){
+
+        //Δήλωση και αρχικοποίηση μεταβλητών
+        int a = 5;
+        int b = 10;
+        int sum = 0;
+        int i = Integer.MAX_VALUE; // int i =  2_147_483_648L;
+        int j = Integer.MIN_VALUE; // int i =  -2_147_483_648L;
+        System.out.println(i);
+
+        long l1 = 4L;
+        long l2 = 8L;
+        long result = 0L;
+
+        long l = 2_147_483_648L;
+
+
+    }
+
+}
